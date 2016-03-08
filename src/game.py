5ae4645d4f741fadf87b0a61dd4ec845ee9f757b
Eielef <eielef@yahoo.com>
2016-03-08 23:15:33 +0100

    Added a different image for flying down.

--- src/game.py
+++ src/game.py
@@ -17,15 +17,17 @@ fps = config['fps']
 
 pygame.init()
 
+# Come up with a more automated system for loading images and animations (images/dragon/fly_up001.png ... )
 guy_down = pygame.image.load('images/dragon1.png')
 guy_up = pygame.image.load('images/dragon2.png')
+guy_flydown = pygame.image.load('images/dragon3.png')
 guy_rect = guy_down.get_rect()
 objectAnimDrawable = AnimDrawable.AnimDrawable('glide', guy_down.get_rect(), {'glide': ({'frame': guy_down, 'time':10},
                                                                                         {'frame': guy_down, 'time':10}),
                                                                               'fly_up':({'frame': guy_up, 'time': 20},
                                                                                         {'frame': guy_down, 'time': 20}),
-                                                                              'fly_down':({'frame': guy_up, 'time': 20},
-                                                                                          {'frame': guy_up, 'time': 20})})
+                                                                              'fly_down':({'frame': guy_flydown, 'time': 20},
+                                                                                          {'frame': guy_flydown, 'time': 20})})
 dragon = Dragon.Dragon(objectAnimDrawable, guy_down.get_rect())
 screen = pygame.display.set_mode(screen_size)
 
