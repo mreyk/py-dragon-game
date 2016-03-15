b85692f9559adbafa6466f7eb8bfb98601316af0
Eielef <eielef@yahoo.com>
2016-03-15 17:35:17 +0100

    Slight renaming.

--- src/game.py
+++ src/game.py
@@ -18,19 +18,19 @@ fps = config['fps']
 pygame.init()
 
 # Come up with a more automated system for loading images and animations (images/dragon/fly_up001.png ... )
-guy_down = pygame.image.load('images/dragon/flyup_001.png')
-guy_up = pygame.image.load('images/dragon/flyup_002.png')
-guy_flydown = pygame.image.load('images/dragon/flydown_001.png')
-guy_fire = pygame.image.load('images/dragon/fire_001.png')
-guy_rect = guy_down.get_rect()
-objectAnimDrawable = AnimDrawable.AnimDrawable('glide', guy_down.get_rect(), {'glide': ({'frame': guy_down, 'time':10},
-                                                                                        {'frame': guy_down, 'time':10}),
-                                                                              'fly_up':({'frame': guy_up, 'time': 20},
-                                                                                        {'frame': guy_down, 'time': 20}),
-                                                                              'fly_down':({'frame': guy_flydown, 'time': 20},
-                                                                                          {'frame': guy_flydown, 'time': 20}),
-                                                                              'fire':({'frame': guy_fire, 'time': 200},
-                                                                                      {'frame':guy_fire, 'time': 200})})
+flyup_001 = pygame.image.load('images/dragon/flyup_001.png')
+flyup_002 = pygame.image.load('images/dragon/flyup_002.png')
+flydown_001 = pygame.image.load('images/dragon/flydown_001.png')
+fire_001 = pygame.image.load('images/dragon/fire_001.png')
+guy_rect = flyup_001.get_rect()
+objectAnimDrawable = AnimDrawable.AnimDrawable('glide', flyup_001.get_rect(), {'glide': ({'frame': flyup_001, 'time':10},
+                                                                                        {'frame': flyup_001, 'time':10}),
+                                                                              'fly_up':({'frame': flyup_002, 'time': 20},
+                                                                                        {'frame': flyup_001, 'time': 20}),
+                                                                              'fly_down':({'frame': flydown_001, 'time': 20},
+                                                                                          {'frame': flydown_001, 'time': 20}),
+                                                                              'fire':({'frame': fire_001, 'time': 200},
+                                                                                      {'frame':fire_001, 'time': 200})})
 guy_rect = pygame.Rect(0,0,100,60)
 dragon = Dragon.Dragon(objectAnimDrawable, guy_rect)
 screen = pygame.display.set_mode(screen_size)
