7dad6c88a38aa5c6fcec680a2c4abac93ffad213
Eielef <eielef@yahoo.com>
2016-07-25 13:12:11 +0200

    Edited some enemy parameters.

--- src/game/EnemySkipper.py
+++ src/game/EnemySkipper.py
@@ -7,25 +7,25 @@ class EnemySkipper(Enemy.Enemy):
 
     def __init__(self, drawable, col_rect):
         Enemy.Enemy.__init__(self, drawable, col_rect)
-        self.state = 'ENEMY'
+        self.state = 'AIMING'
         self.STATES = ('ENEMY', 'DEAD', 'HANGING', 'ATTACK')
         self.xSpeed = -2
     
     def update(self, screen, Debug=False):
         self.counter += 0.1
         
-        if self.counter > self.xpTime and self.state == 'ENEMY':
+        if self.counter > self.xpTime and self.state == 'AIMING':
             self.state = 'HANGING'
             self.xSpeed = 0
             self.ySpeed = 2
             
         if self.y > 50 and self.state == 'HANGING':
-            self.ySpeed = -5
-            self.xSpeed = 0.05
+            self.ySpeed = -4
+            self.xSpeed = 0
             self.state = 'ATTACK'
 
         if self.state == 'ATTACK':
-            self.ySpeed += 0.1
+            self.ySpeed += 0.2
 
         self.y += self.ySpeed
         self.x += self.xSpeed
@@ -38,3 +38,12 @@ class EnemySkipper(Enemy.Enemy):
 
         if (self.drawable.rect.right < 0 or self.drawable.rect.bottom > screen.get_height()):
             self.state = 'DEAD'
+
+    def shoot(self, x, y):
+        self.x = x
+
+        self.col_rect.x = self.x - self.col_rect.width/2
+        self.col_rect.y = self.y - self.col_rect.height/2
+
+        self.drawable.rect.x = self.x - self.drawable.rect.width/2
+        self.drawable.rect.y = self.y - self.drawable.rect.height/2
